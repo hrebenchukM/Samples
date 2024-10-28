c249a6211c7da806079675d3d8609d7df8b116f3
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-10-28 02:49:16 +0200

    ..

--- 004_MultiClients/main.js
+++ 004_MultiClients/main.js
@@ -1,4 +1,5 @@
 // подключение express и socket.io 
+//в данном примере мы подняли несколько клиентов, но в одном интерфейсе поэтому структуру странички просто разделии на 2 части
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -6,14 +7,14 @@ var io = require('socket.io')(server);
 var path = require('path'); 
 
 var port = 8080; 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) {//гет запрос отправляет страничку
     res.sendFile(path.join(__dirname, 'index.html'));
 });
-io.on('connection', function (socket) {    
+io.on('connection', function (socket) {    //подписываемся на событие connection
     // отправка данных клиенту 
-    socket.emit('data', { message: 'data from server' }); 
+    socket.emit('data', { message: 'data from server' }); //сокет генерирует новое событие data и отправляет в обработчик этого события обьект message с текстом 
     // подтверждение получения данных клиентом 
-    socket.on('response', function (data) {
+    socket.on('response', function (data) {//сам подписывается на событие response
         console.log(data.message);
     });
 });
