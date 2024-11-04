2258ddc2f43881099af91f9502ace0f20b6f44a6
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-11-05 01:18:30 +0200

    ..

--- 007_ChatMessages/client.js
+++ 007_ChatMessages/client.js
@@ -1,26 +1,26 @@
 window.onload = function () { 
-
+//принцип сенд чат отправка получение 
     var btn = document.getElementById('btn'); 
     var message_input = document.getElementById('inp'); 
     var message_container = document.getElementById('messages'); 
 
-    var socket = io.connect('http://26.2.14.207:8080');
+    var socket = io.connect('http://26.222.247.153:8080');
 
-    socket.on('chat message', function (message) {
+    socket.on('chat message', function (message) {//все клиенты подписываются на событие сервера chat message
         console.log(message)
-        // сгенерировать html разметку сообщения 
+        // сгенерировать html блок разметку сообщения 
         var display_message = `<div class ="panel well">
                                    <h4>Message: </h4>
                                    <h5>${message.text}</h5>
                                </div>` 
 
-        // добавить результат на страницу 
+        // добавить результат блок на страницу клиента
         message_container.innerHTML += display_message;
 
     })
 
     btn.onclick = function () {
-        // сгенерировать событие отправки сообщения 
+        // сгенерировать событие отправки сообщения при нажатии на кнопку
         socket.emit('send message', { text: message_input.value }); 
 
     }
--- 007_ChatMessages/main.js
+++ 007_ChatMessages/main.js
@@ -1,4 +1,5 @@
 // подключение express и socket.io 
+//принцип сенд чат отправка получение 
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -8,31 +9,31 @@ var path = require('path');
 var port = 8080; 
 
 // массив для хранения текущих подключений 
-var connections = [];
+var connections = [];//нужен чтоб сохранять данные пользователей которые подключаются к нам
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/client.js', function (req, res) {
+app.get('/client.js', function (req, res) {//разделили скрипт от штмл разметки просто , в случае если клиентска программа работает на одном хостинге а сервер на другом хостинге
     res.sendFile(path.join(__dirname, 'client.js')); 
 });
 
 // установка соединения
 io.on('connection', function (socket) {
 
-    connections.push(socket);
-    console.log('Connected: %s sockets connected', connections.length); 
+    connections.push(socket);//socket добавляется в наш массив connections, типо вектор под капотом
+    console.log('Connected: %s sockets connected', connections.length); //показали на стороне сервера что сокет присоединяется
 
     // окончание соединения 
     socket.on('disconnect', function (data) {
-        connections.splice(connections.indexOf(socket), 1);
+        connections.splice(connections.indexOf(socket), 1);//indexOf ищем этот сокет и splice удаляет его и сдвигает освобождает место 
         console.log('Disconnected: %s sockets connected', connections.length); 
     })
 
-    socket.on('send message', function (data) {
-        // сгенерировать событие chat message и отправить его всем доступным подключениям 
-        io.sockets.emit('chat message', data); 
+    socket.on('send message', function (data) {//подписываемся на событие send message от клиента
+        // сгенерировать новое событие chat message и отправить его всем доступным подключениям 
+        io.sockets.emit('chat message', data); // у нас есть массив сокетов, похоже на мультикаст,  всем подписчикам(всем подключеннім сокетам) генерирую событие chat message и передаю всем данные дата- проблема дублирования информации у каждого клиента
     })
 })
 
