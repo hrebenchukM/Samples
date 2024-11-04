369337c500166edcafe75f8be28eba58599ca31f
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-11-05 00:54:36 +0200

    //

--- 005_SocletNamespaces/main.js
+++ 005_SocletNamespaces/main.js
@@ -1,22 +1,24 @@
 // подключение express и socket.io 
 var app = require('express')();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var io = require('socket.io')(server);//сокет ио - это библиотека работающая по событийно-ориентированной модели.То есть клиент и сервер не общаются друг с другом через http протокол(например rest архитектура) . Они тут общаются друг с другом на основе собыйтий(сервер генерирует событие , клиент подписывается на это событие.Клиент обрабатывает событие и генерирует другое событие на которое подписывается сервер )
 
 var path = require('path'); 
 
 var port = 8080; 
 
+//создаем компонент мидлвер для обработки гет запроса
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));//отправили файлик
 });
 
 // пространства имен в socket.io создаются с помощью функции io.of('namespace_name') 
-var namespace = io.of('/namespace'); 
+var namespace = io.of('/namespace'); //создаю пространство имен чтоб изолировать определнные фрагменты друг от друга чтоб не было конфликта при одинаковых идентификаторах 
+
 // подключение к именному пространству socket.io 
-namespace.on('connection', function (socket) {
+namespace.on('connection', function (socket) {//в рамках неймспейса создаю обработчик для события конекшен
     console.log('connected to namespace');
-    // генерация события greet 
+    // генерация нового события под названием  greet 
     namespace.emit('greet', { message: 'Hello from namespace!' });
 
 });
--- 006_SocketRooms/main.js
+++ 006_SocketRooms/main.js
@@ -1,4 +1,5 @@
 ﻿// подключение express и socket.io 
+//пример с тематическими чатами 
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -7,7 +8,7 @@ var path = require('path');
 
 var port = 8080; 
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) {//в целом гет запрос всего приложения
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
@@ -18,15 +19,15 @@ var namespace = io.of('/namespace');
 namespace.on('connection', function (socket) {
     console.log('connected to namespace');
 
-    var room = ''; 
+    var room = ''; //создаем комнату 
     // в каждом пространстве имен может быть выделена отдельная комната(room) - канал связи с произвольным названием  
-    socket.on('btn_click', function (data) {
+    socket.on('btn_click', function (data) {//начинаю обрабатывать событие btn_click от клиента
         if (data.btn == 1) {
 
             room = 'demo room 1'; 
             // подключение к комнате 1
-            socket.join(room);
-            socket.emit('room_join', { roomname: 1 });
+            socket.join(room);//выполняю соединение
+            socket.emit('room_join', { roomname: 1 });//генерирую событие room_join и передаю туда имя 1 
 
         } else if (data.btn == 2) {
 
@@ -36,8 +37,8 @@ namespace.on('connection', function (socket) {
             socket.emit('room_join', { roomname: 2 });
         }
 
-        socket.on('get_greeting', function () {
-            // метод to позволяет генерировать события, которые будут отправлены указанной комнате
+        socket.on('get_greeting', function () {//подписываемся на событие get_greeting
+            // метод to позволяет генерировать события, которые будут отправлены указанной комнате(какая комната будет генерировать события по факту)
             namespace.to('demo room 1').emit('greet', { message: 'Hello from room 1!' });
             namespace.to('demo room 2').emit('greet', { message: 'Hello from room 2!' });
 
